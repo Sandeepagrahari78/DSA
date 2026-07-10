7b9cf7badb8e8c166e1d29cdd0dd90fbd7c136bf
Sandeep agrahari <adityaagrahari890@gmail.com>
2026-07-10 16:38:23 +0530

    Time: 0 ms (100%), Space: 8.6 MB (65.75%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x < 0) return false;
+        int dup = x ; 
+        int rev = 0 ;
+        while(x !=  0) {
+            int digit = x % 10 ;
+            if(rev > INT_MAX/10) return false;
+            rev = (rev * 10 ) + digit ;
+            x = x / 10 ; 
+        }
+        if( rev == dup ) {
+            return true ; 
+        }else{
+            return false ; 
+        }
+    }
+};
